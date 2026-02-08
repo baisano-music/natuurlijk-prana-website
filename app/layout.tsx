e8f939757504341f38fe9ff7bcc24350ecb68787
baisano-music <bas.hof@gmail.com>
2026-02-09 00:05:36 +0100

    Fix MailerLite script - use original loader pattern with stub function

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,15 +26,17 @@ export default async function RootLayout({
         <LayoutWrapper siteSettings={siteSettings}>{children}</LayoutWrapper>
         {/* MailerLite Universal Script */}
         <Script
-          id="mailerlite-universal"
-          strategy="afterInteractive"
-          src="https://assets.mailerlite.com/js/universal.js"
-        />
-        <Script
-          id="mailerlite-init"
+          id="mailerlite"
           strategy="afterInteractive"
           dangerouslySetInnerHTML={{
-            __html: `ml('account', '1116466');`,
+            __html: `
+              (function(w,d,e,u,f,l,n){
+                w[f]=w[f]||function(){(w[f].q=w[f].q||[]).push(arguments);};
+                l=d.createElement(e);l.async=1;l.src=u;
+                n=d.getElementsByTagName(e)[0];n.parentNode.insertBefore(l,n);
+              })(window,document,'script','https://assets.mailerlite.com/js/universal.js','ml');
+              ml('account', '1116466');
+            `,
           }}
         />
       </body>
